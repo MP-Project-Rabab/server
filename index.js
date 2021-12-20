1c3103c75563f4da6a6d3d70952c497771bd31c4
rabab-1993 <rabab.abdulaziz.elq.js@tuwaiq.edu.sa>
2021-12-20 16:01:07 +0300

    create the server file

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const passport = require("passport");
+const dotenv = require("dotenv") 
+dotenv.config();
+const app = express();
+
+
+//  Middlewares
+app.use(express.json({limit: "30mb", extended: true}));
+app. use(express.urlencoded({limit: "30mb", extended: false}))
+app.use(cors());
+app.use(passport.initialize());
+app.use(morgan("dev"));
+
+
+
+
+
+//  PORT 
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`);
+})
