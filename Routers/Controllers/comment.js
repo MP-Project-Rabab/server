65115c055242ed3cb3de561a8d43570fb899cdce
rabab-1993 <rabab.abdulaziz.elq.js@tuwaiq.edu.sa>
2021-12-24 10:26:59 +0300

    added delete comment route and controller

--- Routers/Controllers/comment.js
+++ Routers/Controllers/comment.js
@@ -1,24 +1,23 @@
 const commentModel = require("../../DB/Model/comment");
 
-
 // creat new comment
 const newComment = (req, res) => {
-    const { comment, postId, userId } = req.body;
-    const comments = new commentModel({
-      comment,
-      postId,
-      userId,
+  const { comment, postId, userId } = req.body;
+  const comments = new commentModel({
+    comment,
+    postId,
+    userId,
+  });
+
+  comments
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
     });
-  
-    comments
-      .save()
-      .then((result) => {
-        res.status(201).json(result);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
-  };
+};
 
 // get all comment
 
@@ -33,25 +32,42 @@ const allComment = (req, res) => {
     });
 };
 
-
 // update comment function
 const updateComment = async (req, res) => {
-    const { comment, _id } = req.body;
-    const tokenId = req.saveToken.id;
-    const commentedBy = await commentModel.findOne({ _id });
-    if (tokenId == commentedBy.userId) {
-      await commentModel
-        .findOneAndUpdate({ _id }, { $set: { comment } })
-        .then(() => {
-          res.status(200).json({ massege: "updated successfully" });
-        })
-        .catch((err) => {
-          res.status(400).json(err);
-        });
-    } else {
-      res.status(403).json("forbidden");
-    }
-  };
+  const { comment, _id } = req.body;
+  const tokenId = req.saveToken.id;
+  const commentedBy = await commentModel.findOne({ _id });
+  if (tokenId == commentedBy.userId) {
+    await commentModel
+      .findOneAndUpdate({ _id }, { $set: { comment } })
+      .then(() => {
+        res.status(200).json({ massege: "updated successfully" });
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } else {
+    res.status(403).json("forbidden");
+  }
+};
 
-  module.exports = { newComment, allComment, updateComment, };
-//    deleteComment, 
+// delete comment function
+const deleteComment = async (req, res) => {
+  const { _id } = req.body;
+  const tokenId = req.saveToken.id;
+  const commentedBy = await commentModel.findOne({ _id });
+  if (tokenId == commentedBy.userId) {
+    await commentModel
+      .findByIdAndDelete(_id)
+      .then(() => {
+        res.status(200).json({ massege: "deleted successfully" });
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } else {
+    res.status(403).json("forbidden");
+  }
+};
+
+module.exports = { newComment, allComment, updateComment, deleteComment };
--- Routers/Routes/comment.js
+++ Routers/Routes/comment.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const commentRouter = express.Router();
-const {newComment, updateComment, deleteComment, allComment} = require('../Controllers/comment');
+const {
+  newComment,
+  updateComment,
+  deleteComment,
+  allComment,
+} = require("../Controllers/comment");
 const authentication = require("../midleware/auth");
 
-
-
-
-
-commentRouter.post('/add', authentication, newComment)
-commentRouter.get('/all', authentication, allComment)
-commentRouter.put('/update', authentication, updateComment)
-// commentRouter.delete('/', authentication , deleteComment)
-
-
-
-
-
+commentRouter.post("/add", authentication, newComment);
+commentRouter.get("/all", authentication, allComment);
+commentRouter.put("/update", authentication, updateComment);
+commentRouter.delete("/delete", authentication, deleteComment);
 
 module.exports = commentRouter;
