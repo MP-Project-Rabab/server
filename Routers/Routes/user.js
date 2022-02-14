c5d0c2cabfff2c2df36793472c1a6d7566871b19
rabab-1993 <rabab.abdulaziz.elq.js@tuwaiq.edu.sa>
2022-02-14 19:34:31 +0300

    update

--- Routers/Routes/user.js
+++ Routers/Routes/user.js
@@ -22,7 +22,7 @@ userRouter.get("/activated/:token", activatedAccount);
 userRouter.post("/login", logIn);
 userRouter.get("/profile", profile);
 userRouter.put("/update",authentication, updateProfile);
-userRouter.post("/reset-pass", forgetPass);
+userRouter.get("/reset-pass", forgetPass);
 userRouter.put("/reset-pass/:res-token", updatePassword);
 // // just for admin
 userRouter.put("/user-type", authentication, authorization, updateUserType);
